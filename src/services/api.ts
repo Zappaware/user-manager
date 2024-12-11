c89d77444048ee987a329cd57ee4135d83871328
Sergio Cervantes <sergio_germanc@hotmail.com>
2024-12-11 14:50:19 -0600

    Add the services api to the app

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+// src/services/api.ts
+import axios from 'axios';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+export const fetchUsers = async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+};
