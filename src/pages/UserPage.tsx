f276245f9df97fbda020b78df0a1fb07322b1f09
Sergio Cervantes <sergio_germanc@hotmail.com>
2024-12-12 13:45:15 -0600

    Ignoring a variable for typescript

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -4,6 +4,8 @@ import UserDetails from '../components/UserDetails';
 import { Container } from '@mui/material';
 
 const UserPage: React.FC = () => {
+    // @ts-expect-error id is for other use
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { id } = useParams<{ id: string }>();
 
     return (
